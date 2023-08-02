8b890df4a46796728a5daf3c1c740f843290516c
r-belounis <reza.belounis@gmail.com>
2023-08-02 17:14:09 +0200

    🦺 (fix): Fixing request not found in CI build test coverage

--- tests/utils/jest.setup.ts
+++ tests/utils/jest.setup.ts
@@ -1,6 +1,7 @@
 
 import '@testing-library/jest-dom/extend-expect';
 import { expect } from "@jest/globals";
+import { fetch, Request, Response } from "@remix-run/web-fetch";
 import { toBeOk, toRedirect, toHaveStatus, toHaveHeader, toSetACookie } from "./jest.helpers";
 
 expect.extend({
@@ -9,4 +10,20 @@ expect.extend({
     toHaveStatus,
     toHaveHeader,
     toSetACookie,
-})
+});
+
+// Added Response behavior not working in tests coverage
+// See : https://stackoverflow.com/questions/74497916/referenceerror-request-is-not-defined-when-testing-with-react-router-v6-4
+if (!globalThis.fetch) {
+  // Built-in lib.dom.d.ts expects `fetch(Request | string, ...)` but the web
+  // fetch API allows a URL so @remix-run/web-fetch defines
+  // `fetch(string | URL | Request, ...)`
+  // @ts-expect-error
+  globalThis.fetch = fetch;
+  // Same as above, lib.dom.d.ts doesn't allow a URL to the Request constructor
+  // @ts-expect-error
+  globalThis.Request = Request;
+  // web-std/fetch Response does not currently implement Response.error()
+  // @ts-expect-error
+  globalThis.Response = Response;
+}
