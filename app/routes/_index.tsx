dfc53d44e246a8e725c6ec85e3066bdff659cbc4
r-belounis <reza.belounis@gmail.com>
2023-08-02 02:35:32 +0200

    💚 (fix: done): Vercel deployment -> done
    Changing Vercel settings to right framework was the right deal. Changed url in index to current repository to visit.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,7 +26,7 @@ const Index = () => {
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here you will find my technical test, enjoy 😁</p>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Test made with 💖,<span className="ml-1 italic">by Reza Belounis</span></p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <motion.a initial="default" whileHover="hover" href="https://github.com/r-belounis" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <motion.a initial="default" whileHover="hover" href="https://github.com/r-belounis/yourart-tt" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     See Github repository
                     <motion.svg variants={iconVariants} className="ml-2 -mr-1 w-5 h-5 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></motion.svg>
                 </motion.a>
